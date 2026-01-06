14bcc0b444ea60e4f0d5a7db554f701292d3853c
notfoolish <laszlo.akos@simonyiszki.org>
2026-01-06 12:40:15 +0100

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
+//#region Imports 
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-
+import cors from 'cors';
+import {clerkMiddleware} from "@clerk/clerk-sdk-node";
+import userRoutes from './routes/user.route.js';
+//#endregion
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
 app.get("/", (req, res) => res.send("Hello from the server!"));
+
+app.use("/api/users", userRoutes);
 
 const startServer = async () => {
   try {
